1825bb42d299c126af42e209b970b5236f0ed345
jordanholliday <jordanholliday@gmail.com>
2022-12-09 12:15:09 -0500

    update

---
+++ ingest.go
@@ -0,0 +1,7 @@
+// GOALS
+// 1. Write HTTP endpoint that returns "hello world"
+// 2. Deploy as google Cloud Function
+// 3. Update to write payload to Google Firestore
+// 4. Deploy updated version as google Cloud Function
+
+// starter: https://cloud.google.com/functions/docs/create-deploy-gcloud
